93a5adf9747f673ffff0bdc2c974d88e5513c814
BenoitSlarve98 <benoitslarve98@gmail.com>
2020-03-25 21:18:06 -0400

    TableViewController for Step by Step page
    In this update I added a new view controller class to manage my step by step page.

---
+++ DIY Recipe/DIY Recipe/TableViewController.swift
@@ -0,0 +1,109 @@
+//
+//  TableViewController.swift
+//  DIY Recipe
+//
+//  Created by Slarve N. on 3/25/20.
+//  Copyright © 2020 Slarve N. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    //Var to catch incoming dictionary of instructions data
+    var stepByStepDict = [Int: String]()
+    var keysArray = [Int]()
+    
+    var numbers = [Int]()
+    var instructions = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        //keysArray = Array(stepByStepDict.keys)
+        
+        //print(keysArray)
+        
+        print(numbers.count)
+        print(instructions)
+        
+        tableView.rowHeight = 130
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return numbers.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as? StepByStepCell
+        else{return tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)}
+
+        // Configure the cell...
+        cell.stepNumber.text = "Step: \(numbers[indexPath.row].description)"
+        cell.instructionText.text = instructions[indexPath.row]
+
+        return cell
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
