6b73448c7534b9093d2c95e55839ad00bc28c9f1
BenoitSlarve98 <benoitslarve98@gmail.com>
2020-03-27 15:09:59 -0400

    Login ViewController
    In this update I created a new view controller to manage my login screen.
    I also added a function to segue to the home screen once user credentials are verified.

---
+++ DIY Recipe/DIY Recipe/LoginViewController.swift
@@ -0,0 +1,50 @@
+//
+//  LoginViewController.swift
+//  DIY Recipe
+//
+//  Created by Slarve N. on 3/27/20.
+//  Copyright © 2020 Slarve N. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet var email: UITextField!
+    @IBOutlet var password: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logginTapped(_ sender: Any) {
+        
+        let loginManager = AuthManager()
+        guard let email = email.text, let password = password.text else { return }
+        loginManager.signIn(email: email, pass: password) {[weak self] (success) in
+            guard let `self` = self else { return }
+            var message: String = ""
+            if (success) {
+                self.performSegue(withIdentifier: "login", sender: self)
+            } else {
+                message = "Invalid email or password."
+            }
+            let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.present(alertController, animated: true)
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
